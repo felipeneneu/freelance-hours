e7313c455ac70cc54591d5d9cefc8e04d8ce60c8
felipeneneu <feliperpg3@gmail.com>
2024-10-12 11:06:03 -0300

    attualizaçao layout

--- resources/views/livewire/projects/index.blade.php
+++ resources/views/livewire/projects/index.blade.php
@@ -1,4 +1,4 @@
-<div>
+{{-- <div>
    @foreach ($this->projects as $project)
       
    <li>
@@ -7,4 +7,14 @@
 </a>
    </li>
    @endforeach
+</div> --}}
+<div class="grid grid-cols-2 gap-4">
+   @foreach ($this->projects as $project)
+       <a href="{{ route('projects.show', $project) }}">
+           {{-- <x-projects.simple-card :$project /> --}}
+       </a>
+   @endforeach
 </div>
+{{-- <x-layouts.app>
+   <livewire:projects.index />
+</x-layouts.app> --}}
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,7 +2,7 @@
     
 
    
-    <a href="{{ route('projects.show', 1) }}">Primeiro projeto</a>
+    <a class="bg-[#070715] text-7xl flex flex-col min-h-full text-[#C3C3D1]" href="{{ route('projects.show', 1) }}">Primeiro projeto</a>
 
 
     <livewire:projects.index />
