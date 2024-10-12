fb262dc4119f6bcf25ce7341c2c45ddd7b3446fb
felipeneneu <feliperpg3@gmail.com>
2024-10-12 10:28:22 -0300

    Estrutura pronta

---
+++ resources/views/livewire/projects/show.blade.php
@@ -0,0 +1,13 @@
+<div>
+    meu component show
+
+    <pre>
+        title: {{ $project->title }}
+
+        description: {!! $project->description !!}
+
+
+    </pre>
+        {{-- @dump($project) --}}
+    
+</div>
